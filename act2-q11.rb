98b2aacf96e4ca818f97198ea39d7126b288852d
Nicholas Folis <heffawoozle77@gmail.com>
2024-02-19 13:46:59 -0600

    Modified and completed 'act2-q11.rb', and added an extra note comment to that same file.

--- act2-q11.rb
+++ act2-q11.rb
@@ -3,4 +3,8 @@
 # This program prints out the times an inclusive range is iterated in Ruby.
 
 
-1..5 puts x
+# You can combine ranges to be used like an iterable object.
+
+(1..5).each do |x|
+    puts x
+end
