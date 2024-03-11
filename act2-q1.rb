5a4b26057827210064f46f8e99f4d3582a868554
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-11 13:53:19 -0500

    I have modified and completed 'act2-q1.rb'

--- act2-q1.rb
+++ act2-q1.rb
@@ -2,8 +2,13 @@
 # This program takes text input, and based on the Ruby symbol next to it,
 # converts the text into generated HTML code.
 
+def tag(t_symbol, text)
 
-#tag = lambda {}
+    return "<#{t_symbol}>#{text}</#{t_symbol}>"
+
+end
+
+
 
 puts tag :p, "This is a paragraph."
 puts tag :h1, "This is a header."
