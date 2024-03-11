62d366019938e941cdf5806ed05586dacf70559d
Nicholas Folis <heffawoozle77@gmail.com>
2024-03-10 21:14:00 -0500

    Work began on 'act2-q4.rb', 'act2-q5.rb', and 'act2-q7.rb'. In 'act2-q4.rb', a citation for the 'Ruby-Docs.org' website webpage titled 'class File' was added. 'act2-q5.rb' has been completed.

---
+++ act2-q4.rb
@@ -0,0 +1,9 @@
+
+# This program takes advantage of file operation methods.
+
+
+
+
+# This code below (and the help thereof) is based on the code in
+# the "Ruby-Doc.org" webpage titled "class File".
+
--- act2-q5.rb
+++ act2-q5.rb
@@ -1,2 +1,19 @@
 
 
+class InvalidAgeError < StandardError; end
+
+def validate_age(age)
+    begin
+        if age < 0
+            raise InvalidAgeError, "Age cannot be negative"
+        else
+            puts "Age is valid."
+        end
+    rescue InvalidAgeError => e
+        puts e.message
+    end
+end
+
+
+validate_age(-5)
+validate_age(30)
